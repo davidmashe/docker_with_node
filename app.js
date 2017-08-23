83f2aa68c669aceb8a3a22ce94e2aea7f02c81f2
davidmashe <davidmashe@gmail.com>
2017-08-23 19:52:36 -0400

    string interpolation 'steada concatenation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/",(req, res) => {
-	res.send("Hello from ",process.platform);
+	res.send(`Hello from ${process.platform}`);
 });
 
 app.listen(3000,() => {
